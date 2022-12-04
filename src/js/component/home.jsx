2606ba032656af6a086ba5d95f77313fe4bf0e59
ADOLFO BALSEIRO <abalseiro@hotmail.com>
2022-12-04 17:59:57 +0000

    Empezando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-
+import props from "prop-types";
 
 //include images into your bundle
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div className="watch">
-			<div className="icon"><i class="far fa-clock fa-3x"></i></div>
-			<div className="six"></div>
-			<div className="five"></div>
-			<div className="four"></div>
-			<div className="three"></div>
-			<div className="two"></div>
-			<div className="one"></div>
+			<div className="icon"><i class="far fa-clock"></i></div>
+			<div className="six">{props.six}</div>
+			<div className="five">{props.five}</div>
+			<div className="four">{props.four}</div>
+			<div className="three">{props.three}</div>
+			<div className="two">{props.two}</div>
+			<div className="one">{props.one}</div>
 		</div>
 	);
 };
+
+
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+import props from "prop-types";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
@@ -9,4 +9,8 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+
+setInterval(function(){
+ReactDOM.render(<Home one={counter} one={counter}/>, document.querySelector("#app"));
+},1000);
